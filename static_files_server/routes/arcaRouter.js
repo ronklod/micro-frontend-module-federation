a5c174b91ba0c8d78c96a14bd0e8fed663a03e0f
ronklod <ronklod@gmail.com>
2022-02-11 16:48:17 +0200

    1. adding date details to the server respons

--- static_files_server/routes/arcaRouter.js
+++ static_files_server/routes/arcaRouter.js
@@ -3,7 +3,14 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-    res.status(200).send('getting ARCA details from the server.');
+    var currentdate = new Date();
+    var datetime = currentdate.getDate() + "/"
+        + (currentdate.getMonth()+1)  + "/"
+        + currentdate.getFullYear() + " "
+        + currentdate.getHours() + ":"
+        + currentdate.getMinutes() + ":"
+        + currentdate.getSeconds();
+    res.status(200).send(`[${datetime}] - getting ARCA details from the server.`);
 });
 
 module.exports = router;
