723b168f87f6bdb38d6a399f21e370ac1fea9ed2
Ron Klod <ronklod@users.noreply.github.com>
2022-03-31 17:26:06 +0300

    Update home.jsx

--- home/src/features/home.jsx
+++ home/src/features/home.jsx
@@ -15,6 +15,7 @@ export default () => {
                         <li><a href="https://www.youtube.com/watch?v=njXeMeAu4Sg" target="_blank">https://www.youtube.com/watch?v=njXeMeAu4Sg</a></li>
                         <li><a href="https://www.youtube.com/watch?v=s_Fs4AXsTnA&t=75s">https://www.youtube.com/watch?v=s_Fs4AXsTnA&t=75s</a></li>
                         <li><a href="https://www.npmjs.com/package/create-mf-app" target="_blank">https://www.npmjs.com/package/create-mf-app</a></li>
+                        <li><a href="https://www.youtube.com/watch?v=MU8_LP8R_ZI" target="_blank">Module Federation Into Production</a></li>
                     </ul>
                 </p>
             </div>
